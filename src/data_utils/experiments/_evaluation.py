ff9c21dbc4d068807a6cc8493d5e371d54708b04
conzel <38732545+Conzel@users.noreply.github.com>
2024-09-02 13:13:05 +0200

    CHANGE: Formatting

--- src/data_utils/experiments/_evaluation.py
+++ src/data_utils/experiments/_evaluation.py
@@ -1,11 +1,27 @@
 from typing import Iterable, Literal
 import pandas as pd
+import numpy as np
 
 
-def pareto_front(df: pd.DataFrame, x_column: str, y_column: str, threshhold_values: Iterable[float], mode: Literal["over", "under"]) -> pd.DataFrame:
+def pareto_front(
+    df: pd.DataFrame,
+    x_column: str,
+    y_column: str,
+    mode: Literal["over", "under"],
+    nsteps: int = 100,
+    logspace: bool = False,
+) -> pd.DataFrame:
     pareto_rows = []
-    for acc_threshhold in threshhold_values:
-        mask = df[y_column] < acc_threshhold if mode == "under" else df[y_column] > acc_threshhold
+    val_range = df[y_column].max() - df[y_column].min()
+    if logspace:
+        threshhold_values = np.logspace(
+            np.log10(df[y_column].min()), np.log10(df[y_column].max()), nsteps
+        )
+    else:
+        threshhold_values = np.linspace(df[y_column].min(), df[y_column].max(), nsteps)
+
+    for t in threshhold_values:
+        mask = df[y_column] < t if mode == "under" else df[y_column] > t
         acceptable = df[mask]
         if len(acceptable) == 0:
             continue
