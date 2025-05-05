0e3d8d099ea4abb9f9ea92ab7c17b61da7363b3e
conzel <38732545+Conzel@users.noreply.github.com>
2025-05-05 09:40:39 +0200

    ADD: More timer functionality

--- src/data_utils/experiments/_timer.py
+++ src/data_utils/experiments/_timer.py
@@ -3,12 +3,16 @@ from typing import Iterable, Optional
 
 
 class TimerLoop:
-    def __init__(self, it: Iterable):
+    def __init__(self, it: Iterable, name: Optional[str] = None, disable: bool = False):
         self.it = iter(it)
+        self.disable = disable
+        self.name = name
         self.n = 0
 
     def __iter__(self):
         self.start_time = datetime.datetime.now()
+        if self.name is not None and not self.disable:
+            print(f"Timing loop {self.name}...")
         return self
 
     def __next__(self):
@@ -19,10 +23,24 @@ class TimerLoop:
         except StopIteration:
             self.end_time = datetime.datetime.now()
             elapsed_time = self.end_time - self.start_time
-            print(
-                f"Took {elapsed_time.total_seconds():.2f}s for {self.n} iteration(s), {elapsed_time.total_seconds()/self.n:.2f}s per iteration."
-            )
+            if not self.disable:
+                if self.name is not None:
+                    print(
+                        f"{self.name} took {elapsed_time.total_seconds():.2f}s for {self.n} iteration(s), {elapsed_time.total_seconds()/self.n:.2f}s per iteration."
+                    )
+                else:
+                    print(
+                        f"Took {elapsed_time.total_seconds():.2f}s for {self.n} iteration(s), {elapsed_time.total_seconds()/self.n:.2f}s per iteration."
+                    )
             raise
+
+
+def check_disable(func):
+    def wrapper(self, *args, **kwargs):
+        if not self.disable:
+            return func(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Timer:
@@ -45,9 +63,11 @@ class Timer:
         cls._timers[name].__exit__()
         del cls._timers[name]
 
-    def __init__(self, name: Optional[str] = None):
+    def __init__(self, name: Optional[str] = None, disable: bool = False):
         self.name = name
+        self.disable = disable
 
+    @check_disable
     def __enter__(self):
         if self.name:
             print(f"Timer '{self.name}' started.")
@@ -55,6 +75,7 @@ class Timer:
             print(f"Timer started.")
         self.ts = datetime.datetime.now()
 
+    @check_disable
     def __exit__(self, *args):
         delta = datetime.datetime.now() - self.ts
         if self.name:
